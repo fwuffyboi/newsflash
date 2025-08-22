d7350ed30d58ce377e02bd5d56a504a2caa86dd3
Эшли Карамель <BISCUITISNOTACOOKIE@protonmail.com>
2025-08-22 01:42:25 +0100

    Update open_weather_map.py

--- api/open_weather_map.py
+++ api/open_weather_map.py
@@ -96,11 +96,12 @@ def get_weather_forecast(api_key, location, language, logger):
             f"Error fetching weather forecast data. Status: {response.status_code} --- Response: {response.text}")
 
 
-def get_current_air_quality(api_key, location, logger):
+def get_current_air_quality(api_key, lang, location, logger):
     """
     Fetches the current air quality for a given location using the Google Air Quality API.
 
     :param api_key: OpenWeatherMap API key.
+    :param lang: The language to get the results in.
     :param location: The location for which to fetch the air quality. String or dict with 'latitude' and 'longitude'.
     :param logger:
     :return: A dictionary containing the current air quality data.
@@ -108,12 +109,51 @@ def get_current_air_quality(api_key, location, logger):
 
     lat, lon, location_data = geocodeLocation(api_key, location, logger)
 
-    url = f"https://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
+    url = f"https://api.openweathermap.org/data/2.5/air_pollution?lang={lang}&lat={lat}&lon={lon}&appid={api_key}"
     response = requests.get(url)
 
     if response.status_code == 200:
         logger.info(response.json())  # todo
-        return response.json()
+
+        # figure out the healthiness of the air
+        rating, aqi = rate_air_quality(response.json(), logger)
+        return {
+            "aqi_rating": rating,
+            "aqi": aqi,
+            "location": location,
+            "data": response.json()
+        }
     else:
-        logger.error(f"Error fetching air quality data. Status: {response.status_code} --- Response: {response.text}")
+        logger.error("Error retrieving air quality. Error: {} - {}.".format(response.status_code, response.text))
         return None
+
+
+
+def rate_air_quality(air_response, logger):
+    aqi_list = []
+    # Return a worded rating based on the aqi value
+
+    logger.info(air_response)
+
+    aqi_no = air_response['list'][0]['main']['aqi']
+
+    # Calculate the average aqi value
+    if not aqi_no:
+        return {"error": "No air quality data available."}, 500
+
+    # Determine the air quality rating based on the average aqi value
+    if 0 < aqi_no <= 1:  # if aqi is between 0 and 1
+        caq_rating = "Great!"
+    elif 1 < aqi_no <= 2:  # if aqi is between 1 and 2
+        caq_rating = "Good"
+    elif 2 < aqi_no <= 3:  # if aqi is between 2 and 3
+        caq_rating = "Moderate"
+    elif 3 < aqi_no <= 4:  # if aqi is between 3 and 4
+        caq_rating = "Unhealthy"
+    elif 4 < aqi_no <= 5:  # if aqi is between 4 and 5
+        caq_rating = "Hazardous"
+    else:  # if aqi is greater than 5
+        return {
+            "error": f"Air quality index is too high (over 5). This is likely an API error. AQI: {aqi_no}."}
+
+    return caq_rating, aqi_no
