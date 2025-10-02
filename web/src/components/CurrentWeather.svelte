a91934965c15662008f8d3c9af9cd5a71dac3c38
Эшли Карамель <BISCUITISNOTACOOKIE@protonmail.com>
2025-10-02 01:23:15 +0100

    minor change; needs bugfixing tho

--- web/src/components/CurrentWeather.svelte
+++ web/src/components/CurrentWeather.svelte
@@ -6,8 +6,7 @@
     } from "lucide-svelte";
     import AirQuality from "./AirQuality.svelte";
     import { weatherAlerts } from '../stores/weatherAlerts';
-    let alerts = $state([]);
-    alerts = $weatherAlerts;
+    let alerts = $state($weatherAlerts);
 
     import {onMount} from "svelte";
 
@@ -112,6 +111,7 @@
             <div class="flex flex-row gap-2">
                 <span class="font-bold text-xl">{loc}</span>
                 {#if alerts.length > 0}
+                    <!-- TODO/BUGFIX: doesnt show up sometimes and this is IMPORTANT -->
                     <TriangleAlert class="animate-pulse bg-red-500 rounded-md p-1" size="30" />
                 {/if}
             </div>
