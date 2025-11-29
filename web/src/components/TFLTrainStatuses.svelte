c23a7ab85b0e0c6d1bcda3040d039c3b6768e2fc
Эшли Карамель <BISCUITISNOTACOOKIE@protonmail.com>
2025-11-29 23:30:34 +0000

    Update TFLTrainStatuses.svelte

--- web/src/components/TFLTrainStatuses.svelte
+++ web/src/components/TFLTrainStatuses.svelte
@@ -21,9 +21,7 @@
         fetch("http://192.168.0.226:8080/api/v1/transport/tfl/train-status/", { signal: AbortSignal.timeout(5000) })
             .then(response => response.json())
             .then(data => {
-
                 tfld = data.data;
-
             })
     })
 
