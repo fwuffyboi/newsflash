e440ea9cdf05a547593a0da169f6f25618517b0c
Эшли Карамель <BISCUITISNOTACOOKIE@protonmail.com>
2026-03-06 16:36:47 +0000

    Create SimpleHello.svelte

---
+++ web/src/components/SimpleHello.svelte
@@ -0,0 +1,29 @@
+<script>
+    let name = "Ashley";
+    let message = $state("");
+
+    let t = new Date().getHours();
+
+    if (t > 20) {       message='Good night, '     + name + "!" }
+    else if (t >= 17) { message='Good evening, '   + name + "!" }
+    else if (t >= 12) { message='Good afternoon, ' + name + "!" }
+    else if (t >= 5) {  message='Good morning, '   + name + "!" }
+    else {              message='Good night, '     + name + "!" }
+
+    if (new Date().getMonth() === 11) { //december
+        if (new Date().getDate() === 7) {
+            // it's Ashley's birthday!!! say it's Ashley's birthday :3c.
+            message = "Happy Birthday, " + name + "!!!!"
+        }
+    }
+</script>
+
+<div class="">
+
+    <div class="flex flex-col text-white">
+        <div class="flex flex-row gap-2 text-lg">
+            <span class="font-white">{message}</span>
+        </div>
+    </div>
+
+</div>
