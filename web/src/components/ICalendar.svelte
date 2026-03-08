9c4ec9b2575f9f5a1ffa9f7c78390e512214227b
Эшли Карамель <BISCUITISNOTACOOKIE@protonmail.com>
2026-03-08 01:09:03 +0000

    Update ICalendar.svelte

--- web/src/components/ICalendar.svelte
+++ web/src/components/ICalendar.svelte
@@ -1,23 +1,21 @@
 <script>
     import {onMount} from "svelte";
     import {m} from "$lib/paraglide/messages.js"
-    import {getLocale} from "$lib/paraglide/runtime.js";
     import {CalendarFold} from "lucide-svelte";
 
     let events = $state([
         {
-            "desc": "Loading...",
-            "duration": "24:00:00",
-            "end": "24:00",
+            "desc": "",
+            "duration": "",
+            "end": "",
             "hasEnded": false,
             "location": "",
-            "start": "00:00",
-            "title": "Loading...",
+            "start": "",
+            "title": "Loading events...",
         }
     ]);
-    let ecount = $derived(events.length);
     let message = $state();
-    // let d = new Date();
+    let d = new Date();
 
     onMount(() => {
         fetch("http://localhost:4000/api/v1/ical/", { signal: AbortSignal.timeout(8000) })
@@ -39,6 +37,12 @@
         }
     }
 
+    function allday (start, end) {
+        if (start === "" && end === "") {
+            return "bg-blue-500"
+        }
+    }
+
 </script>
 
 <section class="h-fit w-100 max-w-127 rounded-md bg-gradient-to-tr from-blue-800 to-cyan-900">
@@ -48,40 +52,40 @@
 <!--        <span class="pl-1 underline">{m.ical_today()}, {d.getDate()} {d.toLocaleString(getLocale(), { month: 'long' })} {d.getFullYear()}</span>-->
         <span class="pl-1 underline">{m.ical_today()}</span>
 
-
     </div>
 
-    <div class="flex flex-col rounded-lg pl-4 pb-4 pt-1 text-white">
-        <div class="flex flex-col">
+    <div class="flex flex-col rounded-lg pl-2 pb-4 pt-1 text-white">
+        <div class="flex flex-col max-w-160">
 
             {#each events as e}
-                    <div class="max-w-160 {Coie(e.hasEnded)}">
-                        <div class="flex flex-col">
-                            <div class="flex flex-row justify-between pr-4">
-                                <span class="truncate font-semibold">{e.title}</span>
+                <div class="{Coie(e.hasEnded)} pb-1">
+                    <div class="flex flex-col">
+                        <div class="flex flex-row justify-between pr-4">
 
-                                {#if e.start !== "" && e.end !== ""}
-                                    <span>{e.start} - {e.end}</span>
-                                {/if}
+                            <span class="w-67 truncate font-semibold pl-2 rounded-md {allday(e.start, e.end)}">{e.title}</span>
 
-                            </div>
-
-                            <!--{#if e.desc.length > 0}-->
-                            <!--    <div class="flex flex-row line-clamp-2 italic font-thin">-->
-                            <!--        <span class="font-bold">{m.ical_desc()}:&nbsp;</span>-->
-                            <!--        <span>{e.desc}</span>-->
-                            <!--    </div>-->
-                            <!--{/if}-->
-
-                            {#if e.location.length > 0}
-                                <div class="flex flex-row line-clamp-2 italic font-thin tracking-tighter">
-<!--                                    <span class="font-semibold">{m.ical_location()}:&nbsp;</span>-->
-                                    <span>{e.location}</span>
-                                </div>
+                            {#if e.start !== "" && e.end !== ""}
+                                <span>{e.start} - {e.end}</span>
                             {/if}
 
                         </div>
+
+                        <!--{#if e.desc.length > 0}-->
+                        <!--    <div class="flex flex-row line-clamp-2 italic font-thin">-->
+                        <!--        <span class="font-bold">{m.ical_desc()}:&nbsp;</span>-->
+                        <!--        <span>{e.desc}</span>-->
+                        <!--    </div>-->
+                        <!--{/if}-->
+
+                        {#if e.location.length > 0}
+                            <div class="flex flex-row line-clamp-2 italic font-thin tracking-tighter">
+<!--                                <span class="font-semibold">{m.ical_location()}:&nbsp;</span>-->
+                                <span>{e.location}</span>
+                            </div>
+                        {/if}
+
                     </div>
+                </div>
             {/each}
 
         </div>
